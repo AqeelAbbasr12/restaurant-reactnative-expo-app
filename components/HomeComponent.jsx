81741a9b19fc0d1a9d4f1266e96a266ba98cd683
AqeelAbbasr12 <aqeelabbasr12@gmail.com>
2024-03-18 15:26:46 +0400

    home page menu

--- components/HomeComponent.jsx
+++ components/HomeComponent.jsx
@@ -1,8 +1,12 @@
 import { View } from "react-native";
+import { Link } from "expo-router";
+import { Text } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { customTheme } from "@/utils/theme";
 import { useResponsiveScreen } from "@/hooks/useResponsiveScreen";
 
-export const HomeComponent = ({ children }) => {
-  const { w } = useResponsiveScreen();
+export const HomeComponent = ({ mainLabel, subLabel }) => {
+  const { w, f } = useResponsiveScreen();
 
   return (
     <View
@@ -11,15 +15,35 @@ export const HomeComponent = ({ children }) => {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        backgroundColor: 'linear-gradient(158deg, rgba(83, 83, 83, 1) 0%, rgba(44, 44, 44, 1) 83%)',
-        paddingVertical: w(5),
+        backgroundColor: 'rgba(61, 61, 61) 0%, rgba(44, 44, 44, 1) 83%)',
+        paddingVertical: w(5.3),
         borderTopLeftRadius: 25,
         borderBottomLeftRadius: 25,
         paddingHorizontal: w(5),
         marginVertical: w(3),
+        shadowColor: '#171717',
+        shadowOffset: {width: 3, height: 4},
+        shadowOpacity: 0.4,
+        shadowRadius: 4, 
+        elevation: 5,
+        gradient: true
       }}
     >
-      {children}
+
+      <View style={{display: 'block'}}>
+        <Text style={{ 
+          color: '#f29434', 
+          textTransform: 'uppercase',
+          fontSize: f(2.5), 
+          fontWeight: 800}}>
+            {mainLabel}<Text style={{
+              color: customTheme.colors.textWhite, 
+              textTransform: 'uppercase', 
+              fontSize: f(2.5), 
+              fontWeight: 800}}> {subLabel}</Text></Text>
+        
+      </View>
+        <Icon name="chevron-right" size={f(3)} color={customTheme.colors.iconColorWhite}></Icon>
     </View>
   );
 };
