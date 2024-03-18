fc286fcd0b6408b04c9e3f9286daec72a2b62998
AqeelAbbasr12 <aqeelabbasr12@gmail.com>
2024-03-18 12:16:48 +0400

    home page scroll

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, StatusBar } from "react-native";
 import { Link } from "expo-router";
 import { Screen } from "@/components/Screen";
 import { Heading } from "@/components/Heading";
@@ -6,6 +6,7 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { customTheme } from "@/utils/theme";
 import { useResponsiveScreen } from "@/hooks/useResponsiveScreen";
 import { Header } from "@/components/Header";
+import { CardComponent } from "../components/CardComponent";
 
 const SideItems = () => {
   return (
@@ -18,7 +19,25 @@ const SideItems = () => {
 };
 
 export default function HomePage() {
-  const { w } = useResponsiveScreen();
+  const { w,h,f } = useResponsiveScreen();
+
+  const cardData = [
+    {
+      id: 1,
+      imageSource: require('../assets/images/burger1.png'),
+      title: 'Swiss Mushroom',
+      buttonText: 'Top Seller',
+      buttonTextColor: '#F29434',
+    },
+    {
+      id: 2,
+      imageSource: require('../assets/images/burger1.png'),
+      title: 'Sunny Rocket',
+      buttonText: 'Top Seller',
+      buttonTextColor: '#F29434',
+    },
+    // Add more data as needed
+  ];
 
   return (
     <Screen SideItems={SideItems}>
@@ -44,7 +63,24 @@ export default function HomePage() {
           />
         </View>
       </Header>
+      <View>
+        <ScrollView style={{display: 'flex', flexDirection: 'row', gap: w(4), paddingVertical: h(3)}} horizontal={true} showsHorizontalScrollIndicator={false}>
+        {cardData.map((item, key) => (
+          <View style={{boxShadow: 'rgb(161, 161, 161) 1px 2px 7px 1px',borderRadius: 25, width: 185, marginRight: w(10), marginVertical: h(2)}} key={key}>
+            <CardComponent
+              imageSource={item.imageSource}
+              title={item.title}
+              buttonText={item.buttonText}
+              buttonTextColor="#F29434"
+              onPress={() => console.log('Button pressed')}
+              style={{ marginHorizontal: '10px' }}
+            />
+          </View>
+        ))}
+        </ScrollView>
+      </View>
       <Link href={"/users"}>Go to user</Link>
     </Screen>
   );
+  
 }
--- components/CardComponent.jsx
+++ components/CardComponent.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { customTheme } from "@/utils/theme";
 import { StyleSheet, Image, View } from 'react-native';
 import { Button, Card, Text } from 'react-native-paper';
+import { useResponsiveScreen } from "@/hooks/useResponsiveScreen";
 
 
-const CardComponent = ({imageSource, title, buttonText, buttonTextColor, onPress}) => {
-
+export const CardComponent = ({imageSource, title, buttonText, buttonTextColor, onPress}) => {
+  const { w,h,f } = useResponsiveScreen();
+  
   return (
-    <View style={styles.cardComponentContainer}>
+    
       <Card style={styles.container}>
         
         <View style={styles.imageContainer}>
@@ -19,29 +21,23 @@ const CardComponent = ({imageSource, title, buttonText, buttonTextColor, onPress
           >{buttonText}</Button>
         </View>
         <Card.Content>
-          <Text style={styles.title}>{title}</Text>
+          <Text style={[styles.title, {fontSize: f(2)}]}>{title}</Text>
         </Card.Content>
-        <Image source={require('../assets/images/fire.png')} style={styles.backgroundImage} />
+        <Image source={require('../assets/images/fire2.png')} style={styles.backgroundImage} />
       </Card>
-    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  cardComponentContainer: {
-    boxShadow: 'rgb(161, 161, 161) 1px 2px 7px 1px',
-    borderRadius: 25,
-    width: 185,
-  },
+  
   container: {
     backgroundColor: '#F29434',
     borderRadius: 25,
-    // elevation: 5,
+    elevation: 0,
     shadowColor: 'black',
-    boxShadow: 'rgb(161, 161, 161) 1px 2px 7px 1px',
   },
   title: {
-    fontSize: 17,
+    // fontSize: 17,
     fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
@@ -78,8 +74,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     alignContent: 'center',
-    resizeMode: 'contain',
-    filter: 'brightness(0.85)',  
+    resizeMode: 'contain', 
   },
   buttonLabel: {
     fontSize: 10,
@@ -96,8 +91,7 @@ const styles = StyleSheet.create({
     left: -5,
     width: '55%', 
     height: '55%', 
-    filter: 'contrast(0.6)',
   },
 });
 
-export default CardComponent;
+// export default CardComponent;
